b313f81fb449e516ac36545153e02c65b8b602c3
yuanziheng <710691246@qq.com>
2019-11-19 10:04:05 +0800

    head

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router,Route,NavLink,Redirect} from 'react-router-dom';
+import Home from './views/Home'
+import Login from './views/Login'
+import Register from './views/Register'
 
 function App() {
   return (
-    <div className="App">
-      <h1>地标商城</h1>
+    <div>
+	  <Router>
+		<div>
+			<Redirect to="/home"/>
+			<Route path="/home" component={Home}></Route>
+			<Route path="/login" component={Login}></Route>
+			<Route path="/register" component={Register}></Route>
+			
+		</div>
+	  </Router>
+	  
     </div>
   );
 }
